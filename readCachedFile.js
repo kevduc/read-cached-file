5cf6d6660a1b73dc1474aee9887e045ef4bfb0e3
kevduc <kduconge@gmail.com>
2021-10-16 11:54:36 +0100

    Better doc

--- readCachedFile.js
+++ readCachedFile.js
@@ -3,7 +3,7 @@
 import fs from 'fs'
 
 /**
- * Gets the content for the file when it doesn't exist.
+ * Gets the content for the file.
  * @callback FileContentCallback
  * @returns {string|Promise<string>} Returns the file content. Can also return a Promise resolving to the content.
  */
@@ -11,8 +11,8 @@ import fs from 'fs'
 /**
  * Reads the content of a file if it exists, else gets the content using the {@link getFileContent} function and creates the file.
  * @param {string} path - Path to the file.
- * @param {FileContentCallback} getFileContent - Retrieves/generates the content for the file, which is in general slower and/or more resource/time consuming than reading the local file (e.g. response from a server request, heavy computation).
- * @param {boolean} [forceUpdate=false] - Forces the use of {@link getFileContent} to get the content, and creates the file or overwrittes it if it already exists.
+ * @param {FileContentCallback} getFileContent - Retrieves/generates the content for the file, which is generally slower and/or more resource consuming than reading the local file (e.g. response from a server request, heavy computation).
+ * @param {boolean} [forceUpdate=false] - Forces the use of {@link getFileContent} to get the content, and creates/overwrittes the file.
  * @returns {Promise} Promise object resolving to the data.
  */
 async function readCachedFile(path, getFileContent, forceUpdate = false) {
